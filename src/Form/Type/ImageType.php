0f71fcf0ba6ace0f3122db25a350dde6c7c5358a
benjaminjonard <jonard.benjamin@protonmail.com>
2022-09-27 23:36:35 +0200

    Start implementing autocomplete for tags

--- src/Form/Type/ImageType.php
+++ src/Form/Type/ImageType.php
@@ -5,19 +5,35 @@ declare(strict_types=1);
 namespace App\Form\Type;
 
 use App\Entity\Image;
+use App\Form\Field\TagAutocompleteField;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Routing\RouterInterface;
 
 class ImageType extends AbstractType
 {
+    public function __construct(private readonly RouterInterface $router)
+    {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('file', FileType::class, [
                 'required' => true,
                 'label' => false,
+            ])
+            ->add('tags', TextType::class, [
+                'autocomplete' => true,
+                'tom_select_options' => [
+                    'create' => true,
+                    'createOnBlur' => true,
+                    'delimiter' => ',',
+                ],
+                'autocomplete_url' => $this->router->generate('app_tag_autocomplete')
             ])
         ;
     }
