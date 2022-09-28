8a79a8e3772b471b12bd8aa4c15137df1b1c48f7
Benjamin Jonard <benjaminjonard@users.noreply.github.com>
2022-09-28 23:18:52 +0200

    Handle video preview in upload

--- src/Service/AutomatedTagger.php
+++ src/Service/AutomatedTagger.php
@@ -6,18 +6,37 @@ namespace App\Service;
 
 use App\Entity\Post;
 use App\Repository\TagRepository;
+use FFMpeg\FFMpeg;
 
 class AutomatedTagger
 {
-    public function __construct(private readonly TagRepository $tagRepository)
-    {
+    public function __construct(
+        private readonly TagRepository $tagRepository,
+        private readonly string $publicPath
+    ) {
     }
 
     public function tag(Post $post): void
     {
         if ($post->getMimetype() === 'video/mp4') {
-            $automatedTag = $this->tagRepository->findOneBy(['name' => 'animated']);
-            $post->addTag($automatedTag);
+           $this->animated($post);
+           $this->withSound($post);
+        }
+    }
+
+    private function animated(Post $post): void
+    {
+        $automatedTag = $this->tagRepository->findOneBy(['name' => 'animated']);
+        $post->addTag($automatedTag);
+    }
+
+    private function withSound(Post $post): void
+    {
+        $ffmpeg = FFMpeg::create();
+        $video = $ffmpeg->open($this->publicPath.'/'.$post->getPath());
+        if ($video->getStreams()->audios()->first()) {
+            $withSound = $this->tagRepository->findOneBy(['name' => 'with_sound']);
+            $post->addTag($withSound);
         }
     }
 }
