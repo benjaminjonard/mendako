37ae41402198f6326ad6a4dc37a000acbcd9cd2e
Benjamin Jonard <benjaminjonard@users.noreply.github.com>
2022-09-29 21:53:09 +0200

    Profile

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Form\Type\UserType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class ProfileController extends AbstractController
+{
+    #[Route(path: '/profile', name: 'app_profile_index', methods: ['GET', 'POST'])]
+    public function index(Request $request, TranslatorInterface $translator, ManagerRegistry $managerRegistry): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $managerRegistry->getManager()->flush();
+            $this->addFlash('notice', $translator->trans('message.profile_updated'));
+
+            return $this->redirectToRoute('app_profile_index');
+        }
+
+        return $this->render('App/Profile/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
--- src/Form/Type/PostType.php
+++ src/Form/Type/PostType.php
@@ -10,15 +10,12 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Routing\RouterInterface;
 
 class PostType extends AbstractType
 {
     public function __construct(
-        private readonly RouterInterface $router,
         private readonly StringToTagTransformer $stringToTagTransformer,
     ) {
     }
