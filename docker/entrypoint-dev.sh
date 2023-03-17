3595077c000f7776185bef2e8298c21a247fe61c
Benjamin Jonard <benjamin.jonard@sooyoos.com>
2023-03-17 18:35:26 +0100

    Fix dev entrypoint

--- docker/entrypoint-dev.sh
+++ docker/entrypoint-dev.sh
@@ -2,16 +2,31 @@
 
 set -e
 
-echo "**** Inject .env values ****" && \
-	/inject.sh
+echo "**** Inject .env values ****"
+rm -rf /var/www/mendako/.env.local
+touch /var/www/mendako/.env.local
 
-[ ! -e /tmp/first_run ] && \
-	echo "**** Migrate the database ****" && \
-	cd /var/www/mendako && \
-	php bin/console doctrine:migration:migrate --no-interaction --allow-no-migration --env=prod && \
-	touch /tmp/first_run
+echo "APP_ENV=${APP_ENV:-prod}" >> "/var/www/mendako/.env.local"
+echo "APP_DEBUG=${APP_DEBUG:-0}" >> "/var/www/mendako/.env.local"
+echo "APP_SECRET=${APP_SECRET:-$(openssl rand -base64 21)}" >> "/var/www/mendako/.env.local"
 
-echo "**** Create nginx log files ****" && \
+echo "DB_DRIVER=${DB_DRIVER:-}" >> "/var/www/mendako/.env.local"
+echo "DB_NAME=${DB_NAME:-}" >> "/var/www/mendako/.env.local"
+echo "DB_HOST=${DB_HOST:-}" >> "/var/www/mendako/.env.local"
+echo "DB_PORT=${DB_PORT:-}" >> "/var/www/mendako/.env.local"
+echo "DB_USER=${DB_USER:-}" >> "/var/www/mendako/.env.local"
+echo "DB_PASSWORD=${DB_PASSWORD:-}" >> "/var/www/mendako/.env.local"
+echo "DB_VERSION=${DB_VERSION:-}" >> "/var/www/mendako/.env.local"
+
+echo "session.cookie_secure=${HTTPS_ENABLED}" >> /etc/php/8.2/fpm/conf.d/php.ini
+echo "date.timezone=${PHP_TZ}" >> /etc/php/8.2/fpm/conf.d/php.ini
+
+
+echo "**** Migrate the database ****"
+cd /var/www/mendako && \
+php bin/console doctrine:migration:migrate --no-interaction --allow-no-migration --env=prod
+
+echo "**** Create nginx log files ****"
 mkdir -p /logs/nginx
 chown -R www-data:www-data /logs/nginx
 
