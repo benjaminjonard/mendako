9eae1adfe0f3bf91156e0104cddb65a586191675
benjaminjonard <benjamin.jonard@sooyoos.com>
2023-05-30 14:53:58 +0200

    Add tests

---
+++ tests/App/AdminTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use App\Tests\Factory\UserFactory;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class AdminTest extends WebTestCase
+{
+    use Factories;
+    use ResetDatabase;
+
+    private KernelBrowser $client;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->client->followRedirects();
+    }
+
+    public function test_admin_can_access_administration(): void
+    {
+        // Arrange
+        $user = UserFactory::createOne(['roles' => ['ROLE_ADMIN']])->object();
+        $this->client->loginUser($user);
+
+        // Act
+        $this->client->request('GET', '/admin');
+
+        // Assert
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function test_user_cannot_access_administration(): void
+    {
+        // Arrange
+        $user = UserFactory::createOne(['roles' => ['ROLE_USER']])->object();
+        $this->client->loginUser($user);
+
+        // Act
+        $this->client->request('GET', '/admin');
+
+        // Assert
+        $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+    }
+}
